fd1aebd0806c9dd40c780dde3c41dd81ec786f32
Jarek <jarek@Zipcoders-MacBook-Pro-65.local>
2023-03-17 23:16:28 -0400

    Done Student and Started Tests

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,73 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Student {
+    String firstName;
+    String lastName;
+
+    ArrayList<Double> examScores;
+
+    public Student(String firstName, String lastName, Double[] testScores) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores = new ArrayList<Double>(Arrays.asList(testScores));
+    }
+
+        public String getFirstName () {
+            return firstName;
+        }
+
+        public void setFirstName (String firstName){
+            this.firstName = firstName;
+        }
+
+        public String getLastName () {
+            return lastName;
+        }
+
+        public void setLastName (String lastName){
+            this.lastName = lastName;
+        }
+
+        public ArrayList<Double> getNumberOfTestsTaken () {
+            return getNumberOfTestsTaken();
+        }
+        public String getExamScores () {
+        StringBuilder sb = new StringBuilder();
+            sb.append("Exam Score \n");
+            for (int i = 0; i < examScores.size(); i++) {
+                sb.append("\t Exam" + i+1 + "->" + examScores.get(i) + '\n');
+            }
+
+            return sb.toString();
+        }
+
+        public ArrayList<Double> addExamScore () {
+            examScores.add(Double.valueOf(""));
+            return examScores;
+        }
+
+        public void setExamScore ( int examNumber, double setScore){
+            examScores.set(examNumber, setScore);
+        }
+
+        public double getAverageExam (){
+            double sum = 0;
+                for(Double score : examScores){
+                sum += score;
+            }
+            return sum / examScores.size();
+        }
+
+    @Override
+    public String toString() {
+        return "Student: " + firstName + " " + lastName + '\n' +
+                "Average Score: " + getAverageExam() + '\n' +
+                getExamScores();
+    }
+
+    //String listExamScores = new String(String.valueOf(examScores));
+
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,64 @@
 package io.zipcoder;
 
-public class StudentTest {
+import org.junit.Assert;
+import org.junit.Test;
 
+// Given
+// When
+// Then
+public class StudentTest {
+    @Test
+    public void testConstructor(){
+        // Given
+        String firstName = "Trung";
+        String lastName = "Tran";
+        Double[] testScores = new Double[]{
+                100.00, 115.00, 125.00, 135.00};
+        // When
+        Student student = new Student(firstName, lastName, testScores);
+        // Then
+        Assert.assertEquals(firstName,student.getFirstName());
+        Assert.assertEquals(lastName, student.getLastName());
+        //Assert.assertEquals(testScores, student.getExamScores().toString());
+    }
+
+    @Test
+    public void getFirstName() {
+    }
+
+    @Test
+    public void setFirstName() {
+    }
+
+    @Test
+    public void getLastName() {
+    }
+
+    @Test
+    public void setLastName() {
+    }
+
+    @Test
+    public void getNumberOfTestsTaken() {
+    }
+
+    @Test
+    public void getExamScores() {
+    }
+
+    @Test
+    public void addExamScore() {
+    }
+
+    @Test
+    public void setExamScore() {
+    }
+
+    @Test
+    public void getAverageExam() {
+    }
+
+    @Test
+    public void testToString() {
+    }
 }
